2780ea903b483c295f047e09699644c4b5e70cec
Tuan Huynh <tuanhuynht1998@gmail.com>
2020-05-10 09:30:51 -0400

    heroku guest user patch

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -20,7 +20,7 @@ export const Login = () => {
     return (
         <div className='login-container'>
             <h1>Spotify Shuffler</h1>
-            <a href='http://spotify-magic-shuffler-herokuapp.com/api/login'>sign in with spotify</a>
+            <a href='http://spotify-magic-shuffler.herokuapp.com/api/login'>sign in with spotify</a>
             <p onClick={guestSignIn}>sign in as a guest</p>
         </div>
     )
